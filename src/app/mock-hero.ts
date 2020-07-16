ba9cbbd66209f31eece14fa3ea3fd7f55a433c47
“Moussa <moussa.ndiaye@croesus.com>
2020-07-16 18:06:54 -0400

    Edit and display the hero list

---
+++ src/app/mock-hero.ts
@@ -0,0 +1,18 @@
+
+
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id:4, name:"Alberto"},
+    {id:5, name:"Sunil"},
+    {id:6, name:"Annik"},
+    {id:7, name:"Manu"},
+    {id:8, name:"MOT"},
+    {id:9, name:"Jean Piché"},
+    {id:10, name:"Éric"},
+    {id:11, name:"Louis L'italien"},
+    {id:12, name:"Kriss"},
+    {id:13, name:"Renaud"},
+    {id:14, name:"Adam"},
+    {id:15, name:"Mélissa"}
+];
